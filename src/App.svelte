b58c6e9ef83eb8ac36f7d2ca6d5c13381ea6892d
Brian Birtles <birtles@gmail.com>
2023-09-04 14:04:01 +0900

    chore: format svelte code

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,16 @@
 <script lang="ts">
-import { runIdb } from './idb/run-idb';
-import { runSqlite } from './sqlite/run-sqlite';
+  import { runIdb } from './idb/run-idb';
+  import { runSqlite } from './sqlite/run-sqlite';
 
-let running = false;
+  let running = false;
 
-const inProgress = Symbol('in progress');
-type Result = number | typeof inProgress;
-type Configuration = 'IndexedDB' | 'SQLite OPFS SAH';
+  const inProgress = Symbol('in progress');
+  type Result = number | typeof inProgress;
+  type Configuration = 'IndexedDB' | 'SQLite OPFS SAH';
 
-let results: Record<Configuration, [Result, Result, Result]> = {
-  'IndexedDB': [0, 0, 0],
-  'SQLite OPFS SAH': [0, 0, 0]
-};
-
-async function runTests() {
-  results = {
-    'IndexedDB': [0, 0, 0],
-    'SQLite OPFS SAH': [0, 0, 0]
+  let results: Record<Configuration, [Result, Result, Result]> = {
+    IndexedDB: [0, 0, 0],
+    'SQLite OPFS SAH': [0, 0, 0],
   };
 
   const sources = [
@@ -25,55 +19,66 @@ async function runTests() {
     '/data/2.0.191-3.jsonl',
   ];
 
-  for (let i = 0; i < 3; i++) {
-    results['IndexedDB'][i] = inProgress;
-    const result = await runIdb({
-      batchSize: 2000,
-      source: new URL(sources[i], document.location)
-    });
-    results['IndexedDB'][i] = result;
-  }
+  async function runTests() {
+    results = {
+      IndexedDB: [0, 0, 0],
+      'SQLite OPFS SAH': [0, 0, 0],
+    };
 
-  for (let i = 0; i < 3; i++) {
-    results['SQLite OPFS SAH'][i] = inProgress;
-    const result = await runSqlite({
-      batchSize: 2000,
-      source: new URL(sources[i], document.location)
-    });
-    results['SQLite OPFS SAH'][i] = result;
-  }
-}
+    for (let i = 0; i < 3; i++) {
+      results['IndexedDB'][i] = inProgress;
+      const result = await runIdb({
+        batchSize: 2000,
+        source: new URL(sources[i]!, document.location.toString()),
+      });
+      results['IndexedDB'][i] = result;
+    }
 
+    for (let i = 0; i < 3; i++) {
+      results['SQLite OPFS SAH'][i] = inProgress;
+      const result = await runSqlite({
+        batchSize: 2000,
+        source: new URL(sources[i]!, document.location.toString()),
+      });
+      results['SQLite OPFS SAH'][i] = result;
+    }
+  }
 </script>
 
-<button disabled={running} on:click={() => {
-  if (running) {
-    return;
-  }
+<button
+  disabled={running}
+  on:click={() => {
+    if (running) {
+      return;
+    }
 
-  running = true;
-  runTests().finally(() => { running = false });
-}}>Run test</button>
+    running = true;
+    runTests().finally(() => {
+      running = false;
+    });
+  }}>Run test</button
+>
 
 <p>Results:</p>
 
-<div class=results-table>
-<div>Configuration</div>
-<div>Run #1</div>
-<div>Run #2</div>
-<div>Run #3</div>
-{#each Object.entries(results) as [name, [run1, run2, run3]]}
-  <div>{name}</div>
-  <div>{run1 === inProgress ? '⌛️' : run1 ? `${run1}ms` : '--'}</div>
-  <div>{run2 === inProgress ? '⌛️' : run2 ? `${run2}ms` : '--'}</div>
-  <div>{run3 === inProgress ? '⌛️' : run3 ? `${run3}ms` : '--'}</div>
-{/each}
+<div class="results-table">
+  <div>Configuration</div>
+  <div>Run #1</div>
+  <div>Run #2</div>
+  <div>Run #3</div>
+  {#each Object.entries(results) as [name, [run1, run2, run3]]}
+    <div>{name}</div>
+    <div>{run1 === inProgress ? '⌛️' : run1 ? `${run1}ms` : '--'}</div>
+    <div>{run2 === inProgress ? '⌛️' : run2 ? `${run2}ms` : '--'}</div>
+    <div>{run3 === inProgress ? '⌛️' : run3 ? `${run3}ms` : '--'}</div>
+  {/each}
 </div>
 
 <style>
-.results-table {
-  display: inline-grid;
-  grid-template-columns: auto repeat(3, 1fr);
-  gap: 0.5rem 2rem;
-}
+  .results-table {
+    display: inline-grid;
+    grid-template-columns: auto repeat(3, 1fr);
+    gap: 0.5rem 2rem;
+  }
 </style>
+
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+/// <reference types="svelte" />
 import App from './App.svelte';
 
 const app = new App({
-  target: document.getElementById('container'),
+  target: document.getElementById('container')!,
 });
 
 export default app;
