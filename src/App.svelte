c43902f2aa6a9e84c31700b3b661677ee2cc59da
Brian Birtles <birtles@gmail.com>
2023-09-06 14:45:32 +0900

    feat: make test parameters configurable

--- src/App.svelte
+++ src/App.svelte
@@ -6,39 +6,78 @@
   import RunTime from './RunTime.svelte';
 
   const inProgress = Symbol('in progress');
-  const runs = 5;
   const testConfigurations = {
-    IndexedDB: (source: string) =>
+    'Raw IndexedDB': (source: string) =>
       runIdb({
         batchSize: 2000,
         source: new URL(source, document.location.toString()),
       }),
-    'SQLite OPFS SAH': (source: string) =>
+    'SQLite OPFS SAH (json_each)': (source: string) =>
       runSqlite({
         batchSize: 2000,
         source: new URL(source, document.location.toString()),
       }),
-    'SQLite OPFS SAH (separate index)': (source: string) =>
+    'SQLite OPFS SAH (index table)': (source: string) =>
       runSqlite({
         batchSize: 2000,
         separateIndex: true,
         source: new URL(source, document.location.toString()),
       }),
-    'SQLite OPFS SAH (separate index + triggers)': (source: string) =>
+    'SQLite OPFS SAH (index + triggers)': (source: string) =>
       runSqlite({
         batchSize: 2000,
         separateIndex: true,
         source: new URL(source, document.location.toString()),
         useTriggers: true,
       }),
-    'wa-sqlite': (source: string) =>
+    'wa-sqlite (IDBBatchAtomicVFS)': (source: string) =>
       runWaSqlite({
         batchSize: 2000,
         source: new URL(source, document.location.toString()),
       }),
   };
 
+  //
+  // Dataset
+  //
+
+  const dataSets = {
+    '10k words': '/data/2.0.191-10k.jsonl',
+    '30k words': '/data/2.0.191-30k.jsonl',
+  };
+  let dataset: keyof typeof dataSets = '10k words';
+
+  // Seems like Svelte still doesn't like TypeScript:
+  //
+  // https://www.reddit.com/r/sveltejs/comments/12pc8rk/typescript_in_inline_event_handlers_in_svelte/
+  // https://www.reddit.com/r/sveltejs/comments/t474yo/better_way_to_specify_types_for_eventhandlers/
+  const onChangeDataset = (
+    event: Event & { currentTarget: EventTarget & HTMLSelectElement }
+  ) => {
+    dataset = (event.target as HTMLSelectElement)
+      .value as keyof typeof dataSets;
+  };
+
+  //
+  // Runs
+  //
+
+  let runs = 5;
+  const onChangeRuns = (
+    event: Event & { currentTarget: EventTarget & HTMLSelectElement }
+  ) => {
+    runs = parseInt((event.target as HTMLSelectElement).value, 10);
+  };
+
+  //
+  // Running
+  //
+
   let running = false;
+
+  //
+  // Results
+  //
 
   let results = Object.fromEntries(
     Object.keys(testConfigurations).map((key) => [
@@ -46,6 +85,7 @@
       Array(runs + 1).fill(null),
     ])
   );
+  let resultRuns = 5;
 
   async function runTests() {
     results = Object.fromEntries(
@@ -54,6 +94,7 @@
         Array(runs + 1).fill(null),
       ])
     );
+    resultRuns = runs;
 
     let baseline = null;
     for (const [name, test] of Object.entries(testConfigurations)) {
@@ -63,7 +104,7 @@
 
       for (let i = 0; i < runs; i++) {
         results[name]![i] = inProgress;
-        const result = await test('/data/2.0.191-10k.jsonl');
+        const result = await test(dataSets[dataset]!);
 
         const insert = { dur: result.insertDur, diffMs: 0, diffPercent: 0 };
         if (name !== baseline) {
@@ -113,28 +154,48 @@
   }
 </script>
 
-<button
-  disabled={running}
-  on:click={() => {
-    if (running) {
-      return;
-    }
+<div class="controls">
+  <div>
+    <label for="dataset">Dataset:</label>
+    <select id="dataset" value={dataset} on:change={onChangeDataset}>
+      {#each Object.keys(dataSets) as key}
+        <option value={key}>{key}</option>
+      {/each}
+    </select>
+  </div>
 
-    running = true;
-    runTests().finally(() => {
-      running = false;
-    });
-  }}>Run test</button
->
+  <div>
+    <label for="runs">Runs:</label>
+    <select id="runs" value={runs} on:change={onChangeRuns}>
+      {#each Array.from({ length: 10 }, (_, i) => i + 1) as index}
+        <option value={index}>{index}</option>
+      {/each}
+    </select>
+  </div>
+
+  <button
+    disabled={running}
+    on:click={() => {
+      if (running) {
+        return;
+      }
+
+      running = true;
+      runTests().finally(() => {
+        running = false;
+      });
+    }}>Run test</button
+  >
+</div>
 
 <p>Results:</p>
 
 <div
   class="results-table"
-  style={`grid-template-columns: auto repeat(${runs + 0}, 1fr) auto;`}
+  style={`grid-template-columns: auto repeat(${resultRuns + 0}, 1fr) auto;`}
 >
   <div class="heading">Configuration</div>
-  {#each Array.from({ length: runs }, (_, i) => i + 1) as index}
+  {#each Array.from({ length: resultRuns }, (_, i) => i + 1) as index}
     <div class="heading">Run #{index}</div>
   {/each}
   <div class="heading">Average</div>
@@ -160,9 +221,15 @@
 </div>
 
 <style>
+  .controls {
+    display: flex;
+    gap: 2rem;
+    align-items: center;
+  }
+
   .results-table {
     display: inline-grid;
-    gap: 0.5rem 2rem;
+    gap: 2rem;
   }
 
   .results-table .heading {
